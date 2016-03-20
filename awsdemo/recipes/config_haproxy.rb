9b7c25df214aa90002415358291f04422ec99e8f
Ethan Kim <ethankim67@gmail.com>
2016-03-21 03:21:14 +0900

    update

--- awsdemo/recipes/config_haproxy.rb
+++ awsdemo/recipes/config_haproxy.rb
@@ -1,29 +1,18 @@
 
 tomcat_layer_id = search("aws_opsworks_layer", "name:Tomcat Server").first['layer_id']
 
-tomcat_servers = search("aws_opsworks_instance", "layer_ids:#{tomcat_layer_id}").first
-Chef::Log.info("Private IP: #{tomcat_servers}")
-
-file "/tmp/test" do
-  content "#{tomcat_servers}"
-  mode 0644
-  action :create
-end
-
-
+tomcat_servers = search("aws_opsworks_instance", "layer_ids:#{tomcat_layer_id}")
 
 servers = []
 idx = 1 
 tomcat_servers.each do |s|
-  Chef::Log.info("--- : #{s}")
-  Chef::Log.info("--- : #{s['private_ip]}")
   str = "server app" + idx.to_s + " " + s['private_ip'] + ":8080 check"
   servers << str
   idx = idx + 1
 end
 
-template '/etc/haproxy/haproxy.conf' do
-  source 'haproxy.conf.erb'
+template '/etc/haproxy/haproxy.cfg' do
+  source 'haproxy.cfg.erb'
   mode '0440'
   owner 'root'
   group 'root'
