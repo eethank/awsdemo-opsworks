bc6365646c31b6f93201057d259a2bb48093a72f
Ethan Kim <ethankim67@gmail.com>
2016-03-21 03:09:03 +0900

    update

--- awsdemo/recipes/config_haproxy.rb
+++ awsdemo/recipes/config_haproxy.rb
@@ -2,7 +2,6 @@
 tomcat_layer_id = search("aws_opsworks_layer", "name:Tomcat Server").first['layer_id']
 
 tomcat_servers = search("aws_opsworks_instance", "layer_ids:#{tomcat_layer_id}").first
-#Chef::Log.info("Private IP: #{tomcat_servers[:private_ip]}")
 Chef::Log.info("Private IP: #{tomcat_servers}")
 
 file "/tmp/test" do
@@ -16,7 +15,9 @@ end
 servers = []
 idx = 1 
 tomcat_servers.each do |s|
-  str = "server app" + idx.to_s + " " + s['private_ip'].to_s + ":8080 check"
+  Chef::Log.info("--- : #{s}")
+  Chef::Log.info("--- : #{s['private_ip]}")
+  str = "server app" + idx.to_s + " " + s['private_ip'] + ":8080 check"
   servers << str
   idx = idx + 1
 end
